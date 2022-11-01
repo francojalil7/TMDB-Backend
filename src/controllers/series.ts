f87ee57c9a37a8a6604c7d5dc836e138b830ce7f
Franco Jalil <francojalil7@gmail.com>
2022-10-31 23:58:40 -0300

    add tv series

--- src/controllers/series.ts
+++ src/controllers/series.ts
@@ -1,25 +1,46 @@
 import { NextFunction, Request, Response } from "express";
 import axios from "axios";
 const BASE_URL = `https://api.themoviedb.org/3/discover/tv?${process.env.API_KEY}&query=`;
+const populares = `https://api.themoviedb.org/3/discover/movie?${process.env.API_KEY}?sort_by=popularity.desc`;
 
 export const getSeriesCtrl = async (req: Request, res: Response) => {
   try {
-    const content = await axios.get(`${BASE_URL}`);
+    const content = await axios.get(BASE_URL);
     res.send(content.data.results);
   } catch (error) {
     res.sendStatus(404);
   }
 };
 
+export const getPopularesCtrl = async (req: Request, res: Response) => {
+  try {
+    const recentSeries = await axios.get(
+      `https://api.themoviedb.org/3/tv/popular?${process.env.API_KEY}&language=es&page=1`
+    );
+    res.send(recentSeries.data.results);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+};
+
 export const searchSeriesCtrl = async ({ params }: Request, res: Response) => {
-    const urlSearch = `${BASE_URL}/${params.search}`;
-    
-    try {
-      const content = await axios.get(`${urlSearch}`);
-      res.send(content.data.results);
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(404);
-    }
-  };
-  
+  const urlSearch = `${BASE_URL}/${params.search}`;
+  try {
+    const content = await axios.get(`${urlSearch}`);
+    res.send(content.data.results);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+};
+
+export const topRatedCtrl = async (req: Request, res: Response) => {
+  try {
+    const recentSeries = await axios.get(
+      `https://api.themoviedb.org/3/tv/top_rated?${process.env.API_KEY}&language=es&page=1`
+    );
+    res.send(recentSeries.data.results);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+};
